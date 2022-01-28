28350b7987828b3970d0f25522ad8bad0b79ea81
John Parks <jcparks1984@gmail.com>
2022-01-28 17:02:51 -0500

    functions edit

---
+++ dtgfunctions.py
@@ -0,0 +1,25 @@
+
+def random_destination():
+    import random
+    destination_list = ['Chicago', 'Milwaukee', 'Detroit', 'Indianapolis']
+    print(random.choice(destination_list))
+
+def random_restaurant():
+    import random
+    restaurant_list = ['pizza', 'burgers', 'tacos', 'beer']
+    print(random.choice(restaurant_list))
+
+def random_transportation():
+    import random
+    transportation_list = ['car', 'plane', 'train', 'bus']
+    print(random.choice(transportation_list))
+
+def random_entertainment():
+    import random
+    entertainment_list = ['bar', 'movie', 'play', 'sports game']
+    print(random.choice(entertainment_list))
+
+
+
+
+
