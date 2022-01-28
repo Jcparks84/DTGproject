ca87587c88be29ae3fa1921791920dcd29f3b068
John Parks <jcparks1984@gmail.com>
2022-01-28 16:34:46 -0500

    rough final result

--- Main Project.py
+++ Main Project.py
@@ -1,38 +1,103 @@
-def random_destination():
-    import random
-    destination_list = ['Chicago', 'Milwaukee', 'Detroit', 'Indianapolis']
-    print(random.choice(destination_list))
+import random
+from unittest import result
 
-def random_restaurant():
-    import random
-    restaurant_list = ['pizza', 'burgers', 'tacos', 'beer']
-    print(random.choice(restaurant_list))
+destinations = ['Chicago', 'Milwaukee', 'Detroit', 'Indianapolis']
 
-def random_transportation():
-    import random
-    transportation_list = ['car', 'plane', 'train', 'bus']
-    print(random.choice(transportation_list))
+def random_destination(destination_list):
+    result = random.choice(destination_list)
+    return result
 
-def random_entertainment():
-    import random
-    entertainment_list = ['bar', 'movie', 'play', 'sports game']
-    print(random.choice(entertainment_list))
+final_destination = random_destination(destinations)
+print(final_destination)
 
-print('Welcome to Day Trip Generator!')
-usr_response = input('would you like to use our random trip generator? please answer yes or no. ')
-if(usr_response == 'yes'):
-    print('Great lets get started! ')
-else:
-    print('Come back when you are ready!')
+user_satisfied = False
 
-customer_destination = random_destination
-print('Your first selection is your destination')
-random_destination
+while user_satisfied == False:
+    user_input = input('Are you happy with your destination? Please answer yes or no. ')
+    if user_input == 'no':
+        final_destination = random_destination(destinations)
+        print(final_destination)
+    elif user_input =='yes':
+        user_satisfied == True
+        break
+
+restaurants = ['pizza', 'burgers', 'tacos', 'beer']
+
+def random_restaurant(restaurant_list):
+    result = random.choice(restaurant_list)
+    return result
+
+final_restaurant = random_restaurant(restaurants)
+print(final_restaurant)
+
+user_satisfied = False
+
+while user_satisfied == False:
+    user_input = input('Are you happy with your restaurant? Please answer yes or no. ')
+    if user_input == 'no':
+        final_restaurant = random_restaurant(restaurants)
+        print(final_restaurant)
+    elif user_input == 'yes':
+        user_satisfied == True
+        break
+
+transportation = ['car', 'plane', 'train', 'bus']
+
+def random_transportation(transportation):
+    result = random.choice(transportation)
+    return result
+
+final_trans = random_transportation(transportation)
+print(final_trans)
+
+user_satisfied = False
+
+while user_satisfied == False:
+    user_input = input('Are you happy with your mode of transportation? Please answer yes or no.')
+    if user_input == 'no':
+        final_trans = random_transportation(transportation)
+        print(final_trans)
+    elif user_input == 'yes':
+        user_satisfied == True
+        break
+
+entertainment = ['bar', 'movie', 'play', 'sports game']
+
+def random_entertainment(entertainment):
+    result = random.choice(entertainment)
+    return result
+
+final_entertainment = random_entertainment(entertainment)
+print(final_entertainment)
+
+user_satisfied = False
+
+while user_satisfied == False:
+    user_input = input('Are you happy with your form of entertainment? Please answer yes or no.')
+    if user_input == 'no':
+        final_entertainment = random_entertainment(entertainment)
+        print(final_entertainment)
+    elif user_input == 'yes':
+        user_satisfied == True
+        break
 
 
 
 
 
+
+
+print("Congratulations you're goingto: ")
+print(final_destination)
+
+print("You're eating:")
+print(final_restaurant)
+
+print("your mode of transportation is: ")
+print(final_trans)
+
+print("your entertainment event is: ")
+print(final_entertainment)
 
 
 
--- practice.py
+++ practice.py
@@ -1,3 +1,15 @@
-import random
-transportation_list = ['car', 'plane', 'train', 'bus']
-print(random.choice(transportation_list))
+restaurant_list = ['pizza', 'burgers', 'tacos', 'beer']
+
+def random_restaurant(restaurant_list):
+    result = random.choice(restaurant_list)
+    return result
+
+def random_transportation():
+    import random
+    transportation_list = ['car', 'plane', 'train', 'bus']
+    print(random.choice(transportation_list))
+
+def random_entertainment():
+    import random
+    entertainment_list = ['bar', 'movie', 'play', 'sports game']
+    print(random.choice(entertainment_list))
